258201eddddaeebff45e3f1c9026efdf35cfcb0e
david <davidealvarez1@gmail.com>
2020-03-18 10:27:08 -0700

    installed redux-thunk

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-    
+      <Button color="danger">Danger!</Button>
     </div>
   );
 }
